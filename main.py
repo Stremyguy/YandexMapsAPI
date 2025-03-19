74afcc5362e3ac33e57ae7acb7b658fd2b471518
Stremyguy <stremyguy@gmail.com>
2025-03-19 15:25:49 +0300

    Added the increasing / decreasing map logic

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import requests
 from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from PyQt6.QtGui import QPixmap
+from PyQt6.QtCore import Qt
 
 
 class MainWindow(QMainWindow):
@@ -18,17 +19,20 @@ class MainWindow(QMainWindow):
         self.search()
     
     def setup(self) -> None:
-        self.zoomSpinBox.setValue(21)
+        self.scale_step = 5
+        
+        self.zoomSpinBox.setValue(20)
+        self.zoomSpinBox.setSingleStep(self.scale_step)
         self.lat_lineEdit.setText("-96.839074")
         self.lon_lineEdit.setText("39.767235")
     
     def search(self) -> None:
-        server_address = "https://static-maps.yandex.ru/v1?"
-        api_key = "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13"
-        lat, lon, spn = self.lat_lineEdit.text(), self.lon_lineEdit.text(), self.zoomSpinBox.value()
-        ll_spn = f"ll={lat},{lon}&spn={spn},{spn}"
+        self.server_address = "https://static-maps.yandex.ru/v1?"
+        self.api_key = "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13"
+        self.lat, self.lon, self.spn = self.lat_lineEdit.text(), self.lon_lineEdit.text(), self.zoomSpinBox.value()
+        ll_spn = f"ll={self.lat},{self.lon}&spn={self.spn},{self.spn}"
         
-        map_request = f"{server_address}{ll_spn}&apikey={api_key}"
+        map_request = f"{self.server_address}{ll_spn}&apikey={self.api_key}"
         response = requests.get(map_request)
         
         if not response:
@@ -45,6 +49,14 @@ class MainWindow(QMainWindow):
         self.pixmap = QPixmap(f"data/tmp/{self.map_file}")
         self.map_template.setPixmap(self.pixmap)
     
+    def keyPressEvent(self, event: None) -> None:
+        if event.key() == Qt.Key.Key_PageUp:
+            self.zoomSpinBox.setValue(int(self.zoomSpinBox.value()) - self.scale_step)
+        elif event.key() == Qt.Key.Key_PageDown:
+            self.zoomSpinBox.setValue(int(self.zoomSpinBox.value()) + self.scale_step)
+
+        self.search()
+        
     def closeEvent(self, event: None) -> None:
         os.remove(f"data/tmp/{self.map_file}")
 
